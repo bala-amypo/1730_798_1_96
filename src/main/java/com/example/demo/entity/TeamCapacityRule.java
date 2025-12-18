f72a1d3840426e00299c939fb5400acfbf53a7b6
bala-amypo <bala@amypo.in>
2025-12-18 08:28:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/TeamCapacityRule.java
+++ src/main/java/com/example/demo/entity/TeamCapacityRule.java
@@ -11,7 +11,7 @@ import jakarta.validation.constraints.NotNull;
        uniqueConstraints = {
            @UniqueConstraint(columnNames = "teamName")
        })
-public class TeamCapacityConfig {
+public class TeamCapacityRule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
