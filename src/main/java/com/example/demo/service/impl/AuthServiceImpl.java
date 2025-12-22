df4cda02c4d921847445dc6e1e0dd74fa5514c10
bala-amypo <bala@amypo.in>
2025-12-22 10:22:51 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.demo.service.impl;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.repository.UserAccountRepository;
-import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.AuthService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,8 +12,7 @@ public class AuthServiceImpl implements AuthService {
 
     public AuthServiceImpl(
             UserAccountRepository repo,
-            BCryptPasswordEncoder encoder,
-            JwtTokenProvider jwtTokenProvider) {
+            BCryptPasswordEncoder encoder,) {
     }
 
     @Override
