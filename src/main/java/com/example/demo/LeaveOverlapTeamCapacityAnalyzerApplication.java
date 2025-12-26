e44d6bdd41cdfab1052baec3652eb57d4877db34
bala-amypo <bala@amypo.in>
2025-12-26 15:03:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/LeaveOverlapTeamCapacityAnalyzerApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveOverlapTeamCapacityAnalyzerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveOverlapTeamCapacityAnalyzerApplication.class, args);
+    }
+}
--- src/main/java/com/example/demo/dto/LeaveRequestDto.java
+++ src/main/java/com/example/demo/dto/LeaveRequestDto.java
@@ -1,13 +1,69 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// import java.time.LocalDate;
+import java.time.LocalDate;
 
-// public class LeaveRequestDto {
-//     public Long id;
-//     public String employeeId;
-//     public LocalDate startDate;
-//     public LocalDate endDate;
-//     public String type;
-//     public String status;
-//     public String reason;
-// }
+public class LeaveRequestDto {
+    private Long id;
+    private Long employeeId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String type;
+    private String reason;
+    private String status = "PENDING";
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+    
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+    
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getReason() {
+        return reason;
+    }
+    
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
