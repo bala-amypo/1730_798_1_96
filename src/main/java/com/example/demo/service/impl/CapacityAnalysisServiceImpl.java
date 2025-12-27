837740e50738606276c1bae4075b7833ada73303
bala-amypo <bala@amypo.in>
2025-12-27 18:08:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CapacityAnalysisServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.CapacityAnalysisService; // Make sure this is imported
+import org.springframework.stereotype.Service; // This is required
+
+@Service // <--- THIS IS COMPULSORY
+public class CapacityAnalysisServiceImpl implements CapacityAnalysisService {
+    // ... rest of the code I gave you earlier
+}
